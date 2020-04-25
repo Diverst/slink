cf78abfd8885aa32a3ae293c60b098cdd4803f14
octa8888 <malvin00.com@gmail.com>
2020-04-25 17:32:03 +0700

    add repo

---
+++ js/login.js
@@ -0,0 +1,19 @@
+function login(){
+    var username=$('#email_or_username').val();
+    var password=$('#password').val();
+    $.ajax({
+        url:'controller/login_controller.php',
+        type:'POST',
+        data:{
+            login_check:true,
+            username:username,
+            password:password
+        },
+        success:function(data){
+            console.log(data);
+        },
+        error:function(err){
+            console.log(err);
+        }
+    });
+}
